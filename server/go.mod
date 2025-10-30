5773ffacc911f22eac068d792b88f76c973e85f1
Viktar Nikitsin <nikivictor64@gmail.com>
2025-10-30 19:45:34 +0300

    chore (server): enable CORS for cross-origin requests

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module github.com/nikitinvitya/book_site
 
 go 1.25.1
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+)
--- server/internal/handler/router.go
+++ server/internal/handler/router.go
@@ -3,12 +3,21 @@ package handler
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"net/http"
 )
 
 func InitRoutes(h *Handler) http.Handler {
 	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.RequestID)
