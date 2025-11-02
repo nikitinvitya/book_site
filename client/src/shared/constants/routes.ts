cc39016424a1c0204c3832b578e69b6634ac573c
Viktar Nikitsin <nikivictor64@gmail.com>
2025-11-02 21:44:02 +0300

    feat (ui): implement search by string on favorite books page

--- client/src/shared/constants/routes.ts
+++ client/src/shared/constants/routes.ts
@@ -1,4 +1,4 @@
-const enum AppRoutes {
+export const enum AppRoutes {
   HOME = '/',
   FAVORITES = '/favorites',
 }
--- client/src/widgets/BookList/ui/BookList.tsx
+++ client/src/widgets/BookList/ui/BookList.tsx
@@ -1,6 +1,10 @@
 import classes from './BookList.module.scss'
 import {Book, BookCard} from "@/entities/Book";
-import {useFavorite} from "@/features/lib";
+import {useDebounce, useFavorite} from "@/features/lib";
+import {usePathname} from "next/navigation";
+import {AppRoutes} from "@/shared/constants/routes";
+import {useBookStore} from "@/app/providers/storeProvider";
+import {DEBOUNCE_DELAY} from "@/shared/constants/constants";
 
 interface BookListProps {
   books: Book[];
@@ -9,11 +13,31 @@ interface BookListProps {
 export const BookList = ({books}: BookListProps) => {
 
   const {isFavorite, toggleFavorite} = useFavorite()
+  const pathName = usePathname()
+  const {searchQuery} = useBookStore()
+  const debouncedSearchQuery = useDebounce(searchQuery, DEBOUNCE_DELAY)
+
+  let booksToDisplay = books
+
+  const isFiltered = (book : Book, search: string) => {
+    const lowerCaseSearch = search.toLowerCase()
+
+    const titleMatch = book.title.toLowerCase().includes(lowerCaseSearch)
+
+    const authorMatch = book.author_name?.some(name =>
+    name.toLowerCase().includes(lowerCaseSearch))
+
+    return titleMatch || authorMatch
+  }
+
+  if(pathName === AppRoutes.FAVORITES && debouncedSearchQuery) {
+    booksToDisplay = books.filter((book) => isFiltered(book, debouncedSearchQuery))
+  }
 
   return (
     <div className={classes.bookList}>
-      {books.length
-        ? books.map((book) => (
+      {booksToDisplay.length
+        ? booksToDisplay.map((book) => (
         <BookCard
           book={book}
           key={book.key}
