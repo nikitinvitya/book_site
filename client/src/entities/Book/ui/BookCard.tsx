d06b90f99cecd0df3f11c28e79d762e6b2a6adcc
Viktar Nikitsin <nikivictor64@gmail.com>
2025-11-16 23:31:13 +0300

    feat (ui): implement ui logic to show and hide full book info

--- client/src/entities/Book/ui/BookCard.tsx
+++ client/src/entities/Book/ui/BookCard.tsx
@@ -1,11 +1,14 @@
 import {Book} from '@/entities/Book';
 import classes from './Book.module.scss'
 import Image from 'next/image'
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Button} from "@/shared/ui/Button/Button";
 import NotFavoriteIcon from "@/shared/assets/favorite.svg"
 import FavoriteIcon from "@/shared/assets/favoriteFill.svg"
 import NotFoundIcon from "@/shared/assets/notFound.svg"
+import {useBookStore} from "@/app/providers/storeProvider";
+import {BookDescription} from "@/entities/Book/model/book";
+import {getBookDescription} from "@/shared/api/getBookDescription";
 
 interface BookCardProps {
   book: Book;
@@ -21,33 +24,57 @@ export function BookCard({book, onToggleFavorite, isFavorite}: BookCardProps) {
   const coverURL = coverId
     ? `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`
     : NotFoundIcon.src
+  const {openedBooksKeys, setOpenedBooks} = useBookStore()
+  const [description, setDescription] = useState<string | null>(null)
+
+  const isOpen = openedBooksKeys.includes(book.key)
+
+  useEffect(() => {
+
+    if (isOpen || description == null) {
+      const fetchDescription = async () => {
+        const data: BookDescription = await getBookDescription(book.key)
+        setDescription(data.description || "Description not found")
+      }
+      fetchDescription().catch(() => console.error())
+    }
+
+  }, [isOpen, description]);
 
   return (
     <div className={classes.bookCard}>
-      <div className={classes.coverWrapper}>
-        <Image
-          src={coverURL}
-          alt={`Cover of ${book.title}`}
-          fill
-          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
-          className={classes.cover}
-        />
+      <div className={classes.collapsedBookInfo}>
+        <div className={classes.coverWrapper}>
+          <Image
+            src={coverURL}
+            alt={`Cover of ${book.title}`}
+            fill
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+            className={classes.cover}
+          />
+        </div>
+        <p>Title: {book.title}</p>
+        <span>Author: {authorNames}</span>
+        <span>Year: {book.first_publish_year}</span>
       </div>
-      <p>Title: {book.title}</p>
-      <span>Author: {authorNames}</span>
-      <Button onClick={onToggleFavorite}>
-        {isFavorite
-          ? <Image
-            src={FavoriteIcon.src}
-            fill
-            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
-            alt="Favorite" />
-          : <Image
-            src={NotFavoriteIcon.src}
-            fill
-            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
-            alt="Not favorite"/>}
-      </Button>
+
+      <div className={classes.cardButtons}>
+        <Button onClick={() => setOpenedBooks(book.key)} className={classes.showMoreBtn}>{isOpen ? "Close" : "Show more"}</Button>
+        <Button onClick={onToggleFavorite} className={classes.favoriteBtn}>
+          {isFavorite
+            ? <Image src={FavoriteIcon.src} fill alt="Favorite"/>
+            : <Image src={NotFavoriteIcon.src} fill alt="Not favorite"/>}
+        </Button>
+      </div>
+
+      {isOpen && (
+        <div className={classes.fullBookInfo}>
+          <p>Title: {book.title}</p>
+          <span>Author: {authorNames}</span>
+          <span>Year: {book.first_publish_year}</span>
+          <p className={classes.descriptionText}>{description}</p>
+        </div>
+      )}
     </div>
   )
 }
